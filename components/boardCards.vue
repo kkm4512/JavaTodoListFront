ddbf934e20cedfad9191ae6d864e5a57f8a73f26
kkm4512 <nayoun440@naver.com>
2024-06-24 11:09:34 +0900

    member CRUD, board CRUD success
    END

--- components/boardCards.vue
+++ components/boardCards.vue
@@ -1,19 +1,15 @@
 <template>
-  <div
-    v-for="board in boards"
-    :key="board.id"
-    class="flex w-full justify-center mb-5"
-  >
-    <a
-      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-    >
-      <svg-icon @click="removeBoard(board.id)" type="mdi" :path="path" class="cursor-pointer ml-auto"></svg-icon>
+  <div class="flex w-full justify-center mb-5">
+    <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 truncate">
+      <div class="flex justify-between">
+        <svg-icon @click="updateBoard(board)" type="mdi" :path="paths.update" class="mr-auto cursor-pointer mb-5"></svg-icon>
+        <svg-icon @click="removeBoard(board.id)" type="mdi" :path="paths.x" class="cursor-pointer ml-auto"></svg-icon>
+      </div>
       <div class="text-lg mb-2">제목 : {{ board.title }}</div>
       <ul>
-        <li v-for="(des, i) in board.descriptions" :key="i">
-          내용 : {{ des }}
-        </li>
+        <li v-for="(des, i) in board.descriptions" :key="i">내용 : {{ des }}</li>
       </ul>
+      <UpdateModal v-if="isModalOpen" :board="board" :open="isModalOpen" @update-board="updateBoardInfo"/>
     </a>
   </div>
 </template>
@@ -23,8 +19,15 @@ import SvgIcon from "@jamescoyle/vue-icon";
 import { mdiAlphaX } from "@mdi/js";
 import { HttpMethod } from "~/types/HttpMethod";
 import type { Board } from "~/types/boardType";
+import { mdiUpdate } from "@mdi/js";
+import UpdateModal from "./updateModal.vue";
 
-const path = ref(mdiAlphaX);
+const paths = ref({
+  x: mdiAlphaX,
+  update: mdiUpdate,
+});
+
+const isModalOpen = ref<boolean>(false);
 
 defineComponent({
   components: {
@@ -33,17 +36,34 @@ defineComponent({
 });
 
 const props = defineProps<{
-  boards: Board[];
+  board: Board;
+  open: boolean;
 }>();
 
-const emit = defineEmits(["board-removed"])
+const emit = defineEmits(["board-removed","update-open"]);
 
-async function removeBoard(id: string){
-  const res = await boardFetch(`removeBoard/${id}`,HttpMethod.GET);
-  if (res) {    
-    emit("board-removed",id)
-  } 
+async function removeBoard(id: string) {
+  const res = await boardFetch(`removeBoard/${id}`, HttpMethod.GET);
+  if (res) {
+    emit("board-removed", id);
+  }
+}
+
+function updateBoard(board:Board){
+  isModalOpen.value = !isModalOpen.value
+}
+
+function updateBoardInfo(updateBoard:Board){
+  emit("update-open",updateBoard)
+  isModalOpen.value = !isModalOpen.value
+  
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.truncate {
+  white-space: normal;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <BoardCards :boards="res" @board-removed="handleBoardRemoved" />
+    <div v-for="board in res" :key="board.id" class="flex w-full justify-center mb-5">
+      <BoardCards :board="board" :open="isModalOpen" @board-removed="handleBoardRemoved" @update-open="updateBoard"/>
+  </div>
   </div>
 </template>
 
@@ -9,6 +11,7 @@ import { HttpMethod } from "~/types/HttpMethod";
 import type { Board } from "~/types/boardType";
 
 const res = ref<Board[]>([]);
+const isModalOpen = ref<boolean>(false);
 const response = await boardFetch<Board[]>("findAll", HttpMethod.GET);
 if (response) {
   res.value = response;
@@ -16,6 +19,11 @@ if (response) {
 
 function handleBoardRemoved(id:string){
   res.value = res.value.filter( board => board.id !== id );
+}
+
+function updateBoard(updateBoard:Board){
+  const idx = res.value.findIndex( e =>  e.id ===  updateBoard.id );
+  res.value[idx] = {...updateBoard}
 }
 
 </script>
